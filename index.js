4bc202b1533fd1b1972fa14dd3a6c1f19b48631e
Miriam Peskowitz <miriam5000@gmail.com>
2019-10-03 20:13:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+const submitData = (name, email) => {
+  
+    const url = "http://localhost:3001/users"
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+                name, 
+                email
+            })
+    }
+
+    return fetch(url, configObject)
+        .then(response => response.json())
+        .then(data => {
+            const idList = document.querySelector("#user-id")
+            idList.innerHTML = data.id; 
+        })
+        .catch(error => {
+            const err = document.querySelector("#error")
+            err.innerHTML = error.message; 
+        })
+    }
